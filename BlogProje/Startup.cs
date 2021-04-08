a8a70e6d110d081b1e1c60071079b35f73959a9d
Berdan Atılım Şen <berdan_-_00505@hotmail.com>
2021-04-08 17:21:06 +0300

    Add project files.

---
+++ BlogProje/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogProje.Startup))]
+namespace BlogProje
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
