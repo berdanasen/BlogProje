a9b2011be0223067c4e0c6c6cf1940489d882838
Emre Karaman <karaman.66@hotmail.com>
2021-04-20 15:47:15 +0300

    ShowPost/Helpers

---
+++ BlogProje/Attributes/BreadcrumbAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogProje.Attributes
+{
+    public class BreadcrumbAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public BreadcrumbAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
